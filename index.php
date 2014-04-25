a821eae4d5727c634d77c15847a12dc7a4047e35
haijunq <haijunq@gmail.com>
2014-04-24 21:58:21 -0700

    init

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<!-- START SOCIAL MEDIA WIDGET -->
+<ul id="socialbar">
+<li id="facebook">
+<a target="_blank" href="http://www.facebook.com/username">
+<IMG src="http://www.topproducerwebsite.com/images/site_defaults/generic/facebook.png">
+</a>
+</li>
+</ul>
+<style>#socialbar img {border:0px;}#socialbar li img {width:48px; height:48px;}#socialbar li a:hover {position:relative;top:2px;}#socialbar{padding:0px;list-style: none outside none; margin:0px; position: static;}#socialbar li {display:inline;padding-right:2px;}#socialbar{width:180px;text-align:center;}
+</style><!-- END SOCIAL MEDIA WIDGET -->
